e4e90d9df6818e27552a5d7e8bea79bfc33aa8fb
KIMDAEBOK <eoqhrdlek53@gmail.com>
2022-07-02 23:12:11 +0900

    first commit

---
+++ Untitled1.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+print("hello world!")
