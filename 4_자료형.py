86443910ed841628c6b28e795dcc8ff959cae07d
KIMDAEBOK <eoqhrdlek53@gmail.com>
2022-07-03 23:50:29 +0900

    first commit

---
+++ 4_자료형.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+a_list = [1,2,3,4,5]
+print(a_list)
+print(a_list[0])
+print(a_list[1])
+print(a_list[3])
+
+print(a_list[:2])
+print(a_list[2:])
+print(a_list[:4])
